d1d19d63f16f24087fb495f6bd27aec877185d75
Nazrin Afzal <81605440+NazrinAfzal1997@users.noreply.github.com>
2024-06-16 15:05:59 +0800

    65. Rate Limiting

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,6 +8,10 @@ use App\Models\Review;
 
 class ReviewController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('throttle:reviews')->only('store');
+    }
     /**
      * Display a listing of the resource.
      */
